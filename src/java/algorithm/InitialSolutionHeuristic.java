f62b211f128ac4966515ff94d75d7672e192548b
Nahuel Nostrala Hatz <nahuelnostrala2@gmail.com>
2023-10-02 16:34:43 -0300

    little alteration to instances to be run by simple heuristics

--- src/java/algorithm/InitialSolutionHeuristic.java
+++ src/java/algorithm/InitialSolutionHeuristic.java
@@ -4,8 +4,10 @@ import commons.FeasiblePath;
 import commons.Instance;
 
 import java.util.ArrayList;
+import java.util.BitSet;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Optional;
 import java.util.Set;
 
 public class InitialSolutionHeuristic {
@@ -14,6 +16,61 @@ public class InitialSolutionHeuristic {
 
     public InitialSolutionHeuristic(Instance instance) {
         this.instance = instance;
+    }
+
+    private int getTotalDemand(FeasiblePath path) {
+        return path.getCustomersServed().stream().map(instance::getDemand).reduce(Integer::sum).orElse(0);
+    }
+
+    private Optional<FeasiblePath> computeReplacement(FeasiblePath path1, FeasiblePath path2) {
+        if (getTotalDemand(path1) + getTotalDemand(path2) <= instance.getCapacity()) {
+            FeasiblePath ret = new FeasiblePath();
+            BitSet visited = new BitSet(instance.getNumberOfNodes());
+            int lastNode = instance.getDepot();
+            for (int node : path1.getNodes()) {
+                if (node != instance.getDepot()) {
+                    ret.addNode(node, instance.getEdgeWeight(lastNode, node));
+                    visited.set(node);
+                    lastNode = node;
+                }
+            }
+            for (int node : path2.getNodes()) {
+                if (!visited.get(node)) {
+                    ret.addNode(node, instance.getEdgeWeight(lastNode, node));
+                    visited.set(node);
+                    lastNode = node;
+                }
+            }
+            ret.addCustomers(path1.getCustomersServed());
+            ret.addCustomers(path2.getCustomersServed());
+            return Optional.of(ret);
+        }
+        return Optional.empty();
+    }
+
+    private List<FeasiblePath> mergeHeuristic(List<FeasiblePath> pathsToBeMerged) {
+        BitSet merged = new BitSet(pathsToBeMerged.size());
+        for (int i = 0; i < pathsToBeMerged.size(); i++) {
+            FeasiblePath path1 = pathsToBeMerged.get(i);
+            for (int j = 0; j < i; j++) {
+                FeasiblePath path2 = pathsToBeMerged.get(j);
+                if (!merged.get(i) && !merged.get(j)) {
+                    Optional<FeasiblePath> replacement = computeReplacement(path1, path2);
+                    if (replacement.isPresent()) {
+                        pathsToBeMerged.add(replacement.get());
+                        merged.set(i);
+                        merged.set(j);
+                    }
+                }
+            }
+        }
+        List<FeasiblePath> ret = new ArrayList<>();
+        for (int i = 0; i < pathsToBeMerged.size(); i++) {
+            if (!merged.get(i)) {
+                ret.add(pathsToBeMerged.get(i));
+            }
+        }
+        return ret;
     }
 
     private FeasiblePath createOneRedundantPath() {
@@ -59,6 +116,9 @@ public class InitialSolutionHeuristic {
         }
         currentPath.addNode(depot, instance.getEdgeWeight(lastNode, depot));
         ret.add(currentPath);
+        if (ret.size() > instance.getNumberOfVehicles()) {
+            ret = mergeHeuristic(ret);
+        }
         if (!instance.unusedVehiclesAllowed()) {
             // ret should have length equal to the number of vehicles
             FeasiblePath redundantPath = createOneRedundantPath();
